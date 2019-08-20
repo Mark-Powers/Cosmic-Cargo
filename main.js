8374de33bc4f3caae1dfa2ac92334aeeeccfbc58
Mark Powers <markppowers0@gmail.com>
2019-08-19 21:21:57 -0500

    Add animated space ship on main screen

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var gameInterval, canvas, ctx, width, height;
-var t, gameState, ship, party;
+var t, gameState, ship, party, images, imagesLoaded;
 
 function init() {
     t = 0 // The frame of the game (time basically)
@@ -19,12 +19,37 @@ function init() {
         {name: "Tanner", status: "Good"},
     ]
 }
+function loadImages(imagefiles) {
+    loadcount = 0;
+    loadtotal = imagefiles.length;
+    imagesLoaded = false;
+ 
+    // Load the images
+    var loadedimages = [];
+    for (var i=0; i<imagefiles.length; i++) {
+        // Create the image object
+        var image = new Image();
+ 
+        // Add onload event handler
+        image.onload = function () {
+            loadcount++;
+            if (loadcount == loadtotal) {
+                // Done loading
+                imagesLoaded = true;
+            }
+        };
+        image.src = imagefiles[i];
+        loadedimages[i] = image;
+    }
+    return loadedimages;
+}
 window.onload = function () {
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
     document.addEventListener("keydown", keyPush);
     window.addEventListener('resize', resizeCanvas, false);
     window.addEventListener('orientationchange', resizeCanvas, false);
+    images = loadImages(["Assets/Ship_1.png", "Assets/Ship_2.png"])
     init();
     resizeCanvas();
 }
@@ -39,23 +64,7 @@ function game() {
 function draw() {
     switch(gameState){
         case "title":
-            // background
-            color(3);
-            ctx.fillRect(0, 0, width-2, height-2);
-            // stars
-            color(0);
-            for(var x = 0; x < width; x++){
-                for(var y = 0; y < height; y++){
-                    let chance = 1; // chance% of drawing a star
-                    if(randomInt(100) < chance){
-                        ctx.fillRect(x, y, 1, 1);
-                    }
-                }
-            }
-            // title
-            font(25, "PROJECT 71", 4, 20);
-            font(12, "A space trucking game", 7, 30);
-            font(12, "(press any key)", 30, height - 20);
+            drawTitle();
             break;
         case "main":
             // background
@@ -68,10 +77,17 @@ function draw() {
             ctx.rect(1, 1, width-2, height/2);
             ctx.stroke();
             // ship rectangle
+            drawStars(width, height/2);
             ctx.beginPath();
             ctx.lineWidth = "2";
             ctx.rect(1, height/2, width-2, height/2 -1);
             ctx.stroke();
+            // ship
+            if(t % 6 < 3){
+                ctx.drawImage(images[0], 20, 28);
+            } else {
+                ctx.drawImage(images[1], 20, 28);
+            }
             break;
         case "status":
             // background
@@ -100,6 +116,28 @@ function draw() {
             break;
     }
 }
+function drawStars(w, h){
+    color(1);
+    for(var x = 0; x < w; x++){
+        for(var y = 0; y < h; y++){
+            let chance = 1; // chance% of drawing a star
+            if(randomInt(100) < chance){
+                ctx.fillRect(x, y, 1, 1);
+            } 
+        }
+    }
+}
+function drawTitle(){
+    // background
+    color(3);
+    ctx.fillRect(0, 0, width-2, height-2);
+    // stars
+    drawStars(width, height);
+    // title
+    font(25, "PROJECT 71", 4, 20);
+    font(12, "A space trucking game", 7, 30);
+    font(12, "(press any key)", 30, height - 20);
+}
 function update() {
     switch(gameState){
         case "main":
@@ -111,23 +149,16 @@ function update() {
     t++;
 }
 function keyPush(e) {
-    if(gameState == "title"){
+    if(imagesLoaded && gameState == "title"){
         gameState = "main";
         return;
     }
     switch (e.keyCode) {
         case 37: // left
-            
-            break;
         case 38: // up
-            if (vy == 1) return;
-            
-            break;
         case 39: // right
-            
             break;
         case 40: // down
-            
             break;
         case 90: // z
             break;
@@ -153,22 +184,21 @@ function font(size, what, x, y) {
     ctx.fillText(what, x, y);
 }
 function color(c) {
+    var color = "";
     switch (c){
         case 0:
-            ctx.fillStyle = "#00ff00";
-            ctx.strokeStyle = "#00ff00";
+            color = "#FA0000";
             break;
         case 1:
-            ctx.fillStyle = "#00b300";
-            ctx.strokeStyle = "#00b300";
+            color = "#b4b4b4";
             break;
         case 2:
-            ctx.fillStyle = "#008000";
-            ctx.strokeStyle = "#008000";
+            color = "#fafafa";
             break;
         case 3:
-            ctx.fillStyle = "#003300";
-            ctx.strokeStyle = "#003300";
+            color = "#000000";
             break;
     }
+    ctx.fillStyle = color;
+    ctx.strokeStyle = color;
 }
