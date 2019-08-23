6cc8bdffef54142c001e58d3a33208ce6e8dddf3
Matthew Klawitter <mtklaw153@gmail.com>
2019-08-22 20:12:52 -0400

    Changed exterminate to eliminate

--- events.js
+++ events.js
@@ -29,7 +29,7 @@ function generate_events(){
             [["Surrender",function(ship, party){
                 let exterminated = random_choice(party.filter( el => (el.status != "Dead")));
                 exterminated.status = "Dead"
-                return `The doliks exterminate ${exterminated.name} and move on, finding nothing of interest on your ship.`
+                return `The doliks eliminate ${exterminated.name} and move on, finding nothing of interest on your ship.`
             }], 
             ["Try to resist",function(ship, party){
                 // Higher chance to win if there are more members in your party
@@ -45,7 +45,7 @@ function generate_events(){
                     }
                     talker.status = "Dead";
                     fodder.status = "Dead"
-                    return `${talker.name} tries to convince the doliks to leave. They shout 'EXTERMINATE' and murder ${talker.name} and ${fodder.name}.`
+                    return `${talker.name} tries to convince the doliks to leave. They shout 'ELIMINATE' and murder ${talker.name} and ${fodder.name}.`
                 }
             }]]),
         new SpaceEvent("Space Plague", "During your travels through a nebula, your ship took on foreign bacteria, infecting your crew",
